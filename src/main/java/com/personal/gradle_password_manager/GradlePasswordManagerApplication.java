cd8a536ae42a54b53ee670b226f054202e278751
Helen Tan <helentanmz0909@gmail.com>
2026-08-24 00:34:24 +0800

    initial commit

---
+++ src/main/java/com/personal/gradle_password_manager/GradlePasswordManagerApplication.java
@@ -0,0 +1,13 @@
+package com.personal.gradle_password_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlePasswordManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlePasswordManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/gradle_password_manager/GradlePasswordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.gradle_password_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlePasswordManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
