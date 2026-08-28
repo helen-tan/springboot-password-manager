13e4c4f0d1f67a31041a3b37df89b5c02e82405c
Helen Tan <helentanmz0909@gmail.com>
2026-08-28 14:51:10 +0800

    feat: findByEmail mongo query for UserRepository

--- src/main/java/com/personal/springboot_password_manager/repository/UserRepository.java
+++ src/main/java/com/personal/springboot_password_manager/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.personal.springboot_password_manager.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.personal.springboot_password_manager.model.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
-
+    Optional<User> findByEmail(String email);
 }
