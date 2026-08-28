36af409c94da86f57a7320987ea2ac62231167fb
Helen Tan <helentanmz0909@gmail.com>
2026-08-28 14:29:37 +0800

    test: commented out lines for testing mongo connection

--- src/main/java/com/personal/springboot_password_manager/PasswordManagerApplication.java
+++ src/main/java/com/personal/springboot_password_manager/PasswordManagerApplication.java
@@ -2,6 +2,12 @@ package com.personal.springboot_password_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/* For Mongo Debug */
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.context.ApplicationContext;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.core.env.Environment;
+// import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class PasswordManagerApplication {
@@ -10,4 +16,30 @@ public class PasswordManagerApplication {
 		SpringApplication.run(PasswordManagerApplication.class, args);
 	}
 
+	// @Bean
+	// CommandLineRunner inspectMongo(
+	// ApplicationContext context,
+	// Environment environment) {
+
+	// return args -> {
+	// System.out.println("========== MONGO DEBUG ==========");
+
+	// System.out.println("Environment URI: "
+	// + environment.getProperty("spring.mongodb.uri"));
+
+	// System.out.println("Environment database: "
+	// + environment.getProperty("spring.mongodb.database"));
+
+	// String[] mongoTemplates = context.getBeanNamesForType(MongoTemplate.class);
+
+	// for (String name : mongoTemplates) {
+	// MongoTemplate template = context.getBean(name, MongoTemplate.class);
+
+	// System.out.println("MongoTemplate bean: " + name);
+	// System.out.println("Database: " + template.getDb().getName());
+	// }
+
+	// System.out.println("=================================");
+	// };
+	// }
 }
