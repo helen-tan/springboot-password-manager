5b8c95f2d4af119a5432a67247f82609b690b3ba
Helen Tan <helentanmz0909@gmail.com>
2026-08-30 23:21:08 +0800

    feat: userExistByEmail util method in userService

--- src/main/java/com/personal/springboot_password_manager/service/UserService.java
+++ src/main/java/com/personal/springboot_password_manager/service/UserService.java
@@ -1,8 +1,21 @@
 package com.personal.springboot_password_manager.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.personal.springboot_password_manager.model.User;
+import com.personal.springboot_password_manager.repository.UserRepository;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> userExistByEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        return user;
+    }
 }
